5acf11ad810ce5965ef0f0f3fc769c664293563a
AryaSriva <arsh.0005.ac@gmail.com>
2024-05-15 17:55:46 -0400

    Updated about/home page with more information

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -2,15 +2,15 @@ import { Router } from "express";
 const router = Router();
 
 router.route("/").get(async (req, res) => {
-  return res.render("home");
+  return res.render("home", { title: "Home" });
 });
 
 router.route("/about").get(async (req, res) => {
-  return res.render("about");
+  return res.render("about", { title: "About" });
 });
 
 router.route("/contact").get(async (req, res) => {
-  return res.render("contact");
+  return res.render("contact", { title: "Contact" });
 });
 
 export default router;
