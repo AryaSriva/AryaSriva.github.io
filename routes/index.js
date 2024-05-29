f54bea49728b59b93017aeb863675887aeb86e48
AryaSriva <arsh.0005.ac@gmail.com>
2024-05-29 13:01:51 -0400

    changed file structure and removed backend files since github only hosts static websites

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-import pageRoutes from "./pageRoutes.js";
-
-const constructorMethod = (app) => {
-  app.use("/", pageRoutes);
-  app.use("*", (req, res) => {
-    res.status(404).send({ error: "Not Found" });
-  });
-};
-export default constructorMethod;
