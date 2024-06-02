c92665c60ab27ccf42368167e7e9a3af1b04fded
AryaSriva <arsh.0005.ac@gmail.com>
2024-06-02 18:31:08 -0400

    added more hover events and now when user presses the welcome message, music plays

--- client.js
+++ client.js
@@ -1,4 +1,8 @@
 (function ($) {
+  $("#welcomeMessage").click(function () {
+    let intro = new Audio("dependencies/audio/intro.mp3");
+    intro.play();
+  });
   $("#welcomeMessage").hover(
     function () {
       $(this).css("text-shadow", "0 0 10px #FFFFFF");
@@ -15,6 +19,24 @@
     function () {
       $(this).css("text-shadow", "none");
       $(this).css("box-shadow", "none");
+    }
+  );
+  $("h1").hover(
+    function () {
+      $(this).css("text-shadow", "0 0 10px #FAEBD7");
+    },
+    function () {
+      $(this).css("text-shadow", "none");
+    }
+  );
+  $("#aboutMe").hover(
+    function () {
+      $(this).css("text-shadow", "3px 5px 10px #942442");
+      $(this).css("font-size", "larger");
+    },
+    function () {
+      $(this).css("text-shadow", "none");
+      $(this).css("font-size", "large");
     }
   );
 })(window.jQuery);
